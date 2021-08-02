ebbfa440621aeb21367af44027f09244978d0057
navdeep rana <navdeepr@tifrh.res.in>
2021-08-02 15:38:59 +0530

    Cleanup

--- openACC/fd3d_openacc.f90
+++ openACC/fd3d_openacc.f90
@@ -6,23 +6,25 @@ program fd3d
     integer  :: n(3), num_iters, block_size(3), itime
     real(dp) :: length, dl, dl_inv
     real(dp) :: factors(5)
-    real(dp) :: cpuStart, cpuFinish
+    real(dp) :: timer(2)
     real(dp), dimension(:, :, :), allocatable :: u, du, du_exact
 
     call box_init('der3d.nml')
     call allocate_arrays()
     call initial_condition()
     call set_factors()
-    write (*, *) maxval(du_exact - du)
 
     !$acc data copy(u,du,n,factors)
-    call cpu_time(cpuStart)
+    write(*,*) "Loop begins"
+    call cpu_time(timer(1))
     do itime = 0, num_iters
-        call divergence()
+        call derivative()
     end do
-    call cpu_time(cpuFinish)
+    call cpu_time(timer(2))
+    write(*,*) "Loop ends"
+
     !$acc end data
-    write (*, *) 'time taken :', cpuFinish - cpuStart, maxval(du_exact - du(1:n(1), 1:n(2), 1:n(3)))
+    write (*, *) 'time taken :', timer(2) - timer(1), 'error :', maxval(du_exact - du(1:n(1), 1:n(2), 1:n(3)))
 
 contains
 
@@ -60,16 +62,32 @@ contains
         u(:, :, :) = 0.d0
         du_exact(:, :, :) = 0.d0
 
-        do k = -1, n(3) + 2
+        do k = 1, n(3)
             z = dl * (k - 1)
-            do j = -1, n(2) + 2
+            do j = 1, n(2)
                 y = dl * (j - 1)
-                do i = -1, n(1) + 2
+                do i = 1, n(1)
                     x = dl * (i - 1)
                     u(i, j, k) = sin(x) + sin(y) + sin(z)
                 end do
             end do
         end do
+
+        ! Periodic boundary conditions
+        u(0,:,:)      = u(n(1),:,:)
+        u(-1,:,:)     = u(n(1)-1,:,:)
+        u(n(1)+1,:,:) = u(1,:,:)
+        u(n(1)+2,:,:) = u(2,:,:)
+
+        u(:,0,:)      = u(:,n(2),:)
+        u(:,-1,:)     = u(:,n(2)-1,:)
+        u(:,n(2)+1,:) = u(:,1,:)
+        u(:,n(2)+2,:) = u(:,2,:)
+
+        u(:,:,0)      = u(:,:,n(3))
+        u(:,:,-1)     = u(:,:,n(3)-1)
+        u(:,:,n(2)+1) = u(:,:,1)
+        u(:,:,n(2)+2) = u(:,:,2)
 
         do k = 1, n(3)
             z = dl * (k - 1)
@@ -83,19 +101,33 @@ contains
         end do
     end subroutine initial_condition
 
-    subroutine divergence()
+    subroutine derivative()
         integer  :: i, j, k
 
-        #ifdef MOD_APPROACH
+        !$acc parallel loop collapse(3) present(u,du,n,factors)
+        do k = 1, n(3)
+            do j = 1, n(2)
+                do i = 1, n(1)
+                    du(i, j, k) = (u(i - 2, j, k) + u(i, j - 2, k) + u(i, j, k - 2)) * factors(1) + &
+                        (u(i - 1, j, k) + u(i, j - 1, k) + u(i, j, k - 1)) * factors(2) + &
+                        (u(i, j, k) + u(i, j, k) + u(i, j, k)) * factors(3) + &
+                        (u(i + 1, j, k) + u(i, j + 1, k) + u(i, j, k + 1)) * factors(4) + &
+                        (u(i + 2, j, k) + u(i, j + 2, k) + u(i, j, k + 2)) * factors(5)
+                end do
+            end do
+        end do
+        !$acc end parallel
+    end subroutine derivative
+
+    subroutine derivative_mod()
+        integer :: i, j, k
         integer :: l, xn(5), yn(5), zn(5)
-        #endif
 
         !$acc parallel loop collapse(3) present(u,du,n,factors)
         do k = 1, n(3)
             do j = 1, n(2)
                 do i = 1, n(1)
 
-                    #ifdef MOD_APPROACH
                     do l = -2, 2
                         xn(l + 3) = mod(i + l + n(1) - 1, n(1)) + 1
                         yn(l + 3) = mod(j + l + n(2) - 1, n(2)) + 1
@@ -107,17 +139,10 @@ contains
                         (u(xn(3), j, k) + u(i, yn(3), k) + u(i, j, zn(3))) * factors(3) + &
                         (u(xn(4), j, k) + u(i, yn(4), k) + u(i, j, zn(4))) * factors(4) + &
                         (u(xn(5), j, k) + u(i, yn(5), k) + u(i, j, zn(5))) * factors(5)
-                    #else
-                    du(i, j, k) = (u(i - 2, j, k) + u(i, j - 2, k) + u(i, j, k - 2)) * factors(1) + &
-                        (u(i - 1, j, k) + u(i, j - 1, k) + u(i, j, k - 1)) * factors(2) + &
-                        (u(i, j, k) + u(i, j, k) + u(i, j, k)) * factors(3) + &
-                        (u(i + 1, j, k) + u(i, j + 1, k) + u(i, j, k + 1)) * factors(4) + &
-                        (u(i + 2, j, k) + u(i, j + 2, k) + u(i, j, k + 2)) * factors(5)
-                    #endif
 
                 end do
             end do
         end do
         !$acc end parallel
-    end subroutine divergence
+    end subroutine derivative_mod
 end program fd3d
